2b837f0ab3b3b99540db98845ea189d635eb2408
rihito0907 <rihito0907@gmail.com>
2015-03-30 09:29:41 +0900

    storyboardを追加

---
+++ QuickStartApp_iOS/HueQuickStartApp-iOS/HUVTopViewController.h
@@ -0,0 +1,13 @@
+//
+//  HUVTopViewController.h
+//  HueQuickStartApp-iOS
+//
+//  Created by Hashido Rihito on 3/30/15.
+//  Copyright (c) 2015 Philips. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HUVTopViewController : UIViewController
+
+@end
---
+++ QuickStartApp_iOS/HueQuickStartApp-iOS/HUVTopViewController.m
@@ -0,0 +1,37 @@
+//
+//  HUVTopViewController.m
+//  HueQuickStartApp-iOS
+//
+//  Created by Hashido Rihito on 3/30/15.
+//  Copyright (c) 2015 Philips. All rights reserved.
+//
+
+#import "HUVTopViewController.h"
+
+@interface HUVTopViewController ()
+
+@end
+
+@implementation HUVTopViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
