4a7218865183aeb2a6d3f7ef4e54a97a6ae6b140
tgcshta <annorhtht@gmail.com>
2015-03-30 10:53:25 +0900

    Add tweet api.

---
+++ QuickStartApp_iOS/HueQuickStartApp-iOS/TweetManager.h
@@ -0,0 +1,17 @@
+//
+//  Tweet.h
+//  HueQuickStartApp-iOS
+//
+//  Created by sho on 2015/03/30.
+//  Copyright (c) 2015年 Philips. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Social/Social.h>
+#import <Accounts/Accounts.h>
+
+@interface TweetManager : NSObject
+
+- (void)generateSearchRequest;
+
+@end
